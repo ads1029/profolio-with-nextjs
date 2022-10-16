54a7cc023afadee134f6c7319925447dc6f167a4
Minjie Huang <minjie.huang@tigerspike.com>
2022-10-17 05:02:27 +1100

    update nav to contact and about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-
+import Nav from "src/components/Nav";
 const About = () => {
   return (
-    <div className="prose">
-      <h1>Minjie Huang</h1>
-      <div className="flex flex-row">
-        <div className="bg-gradient-to-br from-primary w-80 h-80 col-start-1  shadow-xl" />
-        <div className="bg-gradient-to-tl from-accent to-zinc-100 w-80 h-80 col-start-2 -ml-20 mt-20 shadow-xl" />
+    <div>
+      <Nav></Nav>
+      <div className="prose">
+        <h1>h1 title</h1>
+        <div className="flex flex-row">
+          <div className="bg-gradient-to-br from-primary w-80 h-80 col-start-1  shadow-xl" />
+          <div className="bg-gradient-to-tl from-accent to-zinc-100 w-80 h-80 col-start-2 -ml-20 mt-20 shadow-xl" />
+        </div>
+        <h2>h2</h2>
+        <h3>h3</h3>
       </div>
-      <h2>h2</h2>
-      <h3>h3</h3>
     </div>
   );
 };
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Nav from "src/components/Nav";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <Nav />
+      Contact
+    </div>
+  );
 };
 
 export default Contact;
