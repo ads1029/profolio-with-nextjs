a80cf35a5df49dcfad3068620f63fa8643b86198
Minjie Huang <minjie.huang@tigerspike.com>
2022-10-19 17:17:27 +1100

    follow up props structure

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,27 +2,50 @@ import React from "react";
 import Image from "next/image";
 import Tick from "../../public/check_circle_icon.svg";
 
-const Project = ({ title, summary, techStacks, githubLink, reverse }) => {
+const Project = ({
+  title,
+  summary,
+  techStacks,
+  githubLink,
+  projectImage,
+  reverse,
+}) => {
+  const projectImageDiv = (
+    <div className="bg-gradient-to-br from-primary w-80 h-80 flex-1" />
+  );
+
+  // console.log(techStacks);
+  // console.log(techStacks.map((techStack) => console.log(techStack)));
+
   return (
-    <div className="flex flex-row items-center mb-10 bg-base prose shadow-lg rounded-lg">
-      {reverse || <div className="bg-slate-600 w-80 h-40 mr-10" />}
-      <div className="flex flex-col">
-        <h3 className="mb-1 ">{title}</h3>
-        <p className="mb-1">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-          pariatur ipsa maiores quod similique unde asperiores ut tempora dolor.
-          Obcaecati?
+    <div className="flex flex-row items-center mb-10 bg-base prose shadow-lg rounded-lg lg:prose-xl">
+      {reverse || projectImageDiv}
+      <div className="flex flex-col flex-1 mx-2">
+        <h3>{title}</h3>
+        <p>
+          {summary}
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos illo
+          itaque assumenda id saepe ducimus doloribus inventore repellendus,
+          maiores tenetur.
         </p>
-        <a href="" className="mb-1">
+
+        <a href={githubLink} className="mb-2">
           repo
         </a>
 
         <div className="flex mb-4">
-          <Image src={Tick} alt="tick" className="fill-slate-100" />
-          <div className="mx-4 ">Lorem, ipsum.</div>
+          {techStacks.map((techStack) => {
+            return (
+              <>
+                <Image src={Tick} alt="tick" className="fill-slate-100" />
+                <div className="mx-4 ">{techStack}</div>
+              </>
+            );
+          })}
         </div>
       </div>
-      {reverse && <div className="bg-slate-600 w-80 h-40" />}
+      {reverse && projectImageDiv}
     </div>
   );
 };
