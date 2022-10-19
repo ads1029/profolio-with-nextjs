ac382ea686ec1fb36d0e8eef9edfd5251ad26386
Minjie Huang <minjie.huang@tigerspike.com>
2022-10-19 17:17:13 +1100

    expand project title to project obj

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -4,11 +4,42 @@ import Project from "../../src/components/Project";
 
 const Projects = () => {
   const projectList = [
-    "testing",
-    "GYG",
-    "Several Shopify Projects",
-    "UTMS",
-    "UPhoto",
+    {
+      title: "testing",
+      summary: "testing summary",
+      techStacks: ["a", "vv", "ccc"],
+      githubLink: "https://github.com/",
+    },
+    {
+      title: "GYG",
+      summary: "GYG summary",
+      techStacks: ["a", "vv", "ccc"],
+      githubLink: "https://github.com/",
+    },
+    {
+      title: "Several Shopify Projects",
+      summary: "testing summary",
+      techStacks: ["a", "vv", "ccc"],
+      githubLink: "https://github.com/",
+    },
+    {
+      title: "UTMS",
+      summary: "testing summary",
+      techStacks: ["a", "vv", "ccc"],
+      githubLink: "https://github.com/",
+    },
+    {
+      title: "UPhoto",
+      summary: "testing summary",
+      techStacks: ["a", "vv", "ccc"],
+      githubLink: "https://github.com/",
+    },
+    {
+      title: "Pure Portfolio",
+      summary: "testing summary",
+      techStacks: ["HTML", "CSS"],
+      githubLink: "https://ads1029.github.io/",
+    },
   ];
   return (
     <>
@@ -17,14 +48,15 @@ const Projects = () => {
         {projectList.map((project, index) => {
           return (
             <Project
-              key={`${project}-{index}`}
-              title={project}
+              key={`${project.title}-${index}`}
+              title={project.title}
+              summary={project.summary}
+              techStacks={project.techStacks}
+              githubLink={project.githubLink}
               reverse={!!(index % 2)}
             />
           );
         })}
-        <Project title="testing" reverse />
-        <Project title="testing" />
       </div>
     </>
   );
