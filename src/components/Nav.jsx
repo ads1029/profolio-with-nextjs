f51d6b6ae377fd1900ca2e0631e7c1b7147334c9
Minjie Huang <minjie.huang@tigerspike.com>
2022-10-20 10:56:04 +1100

    update navbar hover animation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-primary mb-2 sticky top-0">
+    <div className="navbar bg-primary mb-2 sticky top-0 z-50">
       <div className="flex-1 hidden sm:block">
         <Link href="/">
           <button className="btn btn-ghost no-animation normal-case text-4xl font-light text-base-100">
@@ -15,22 +15,30 @@ const Navbar = () => {
         <ul className="menu menu-horizontal p-0">
           <li>
             <Link href="/projects">
-              <a className="text-base-100">Projects</a>
+              <a className="text-base-300 hover:text-base-100 duration-500">
+                Projects
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/about">
-              <button className="text-base-100">About</button>
+              <button className="text-base-300 hover:text-base-100 duration-500">
+                About
+              </button>
             </Link>
           </li>
           <li>
             <Link href="/counter">
-              <a className="text-base-100">Counter</a>
+              <a className="text-base-300 hover:text-base-100 duration-500">
+                Counter
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/dog">
-              <a className="text-base-100">Dog API</a>
+              <a className="text-base-300 hover:text-base-100 duration-500">
+                Dog API
+              </a>
             </Link>
           </li>
         </ul>
