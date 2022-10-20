962c6876217e682717191ecef02a5289c5d34def
Minjie Huang <minjie.huang@tigerspike.com>
2022-10-20 11:01:45 +1100

    update import

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Navbar from "../../src/components/Nav";
 import Project from "../../src/components/Project";
+import SakamotoImage from "../../public/projectImages/Sakamoto.webp";
+import GYGImage from "../../public/projectImages/GYG-App-1.jpg";
+import BilliniImage from "../../public/projectImages/portfolio-shopify-billini.png";
+import UTMSImage from "../../public/projectImages/portfolio-UTMS.png";
+import UPhotoImage from "../../public/projectImages/portfolio-Uphoto.png";
+import purePortfolioImage from "../../public/projectImages/portfolio-pure-portfolio.png";
 
 const Projects = () => {
   const projectList = [
@@ -8,42 +14,42 @@ const Projects = () => {
       title: "testing",
       summary: "testing summary",
       techStacks: ["a", "vv", "ccc"],
-      projectImage: "/../public/projectImages/Sakamoto.webp",
+      projectImage: SakamotoImage,
       display: "https://ads1029.github.io/",
     },
     {
       title: "GYG",
       summary: "GYG summary",
       techStacks: ["a", "vv", "ccc"],
-      projectImage: "/../public/projectImages/GYG-App-1.jpg",
+      projectImage: GYGImage,
       display: "https://ads1029.github.io/",
     },
     {
       title: "Billini",
       summary: "testing summary",
       techStacks: ["a", "vv", "ccc"],
-      projectImage: "/../public/projectImages/portfolio-shopify-billini.png",
+      projectImage: BilliniImage,
       display: "https://www.billini.com/",
     },
     {
       title: "UTMS",
       summary: "testing summary",
       techStacks: ["a", "vv", "ccc"],
-      projectImage: "/../public/projectImages/portfolio-UTMS.png",
+      projectImage: UTMSImage,
       display: "https://ads1029.github.io/",
     },
     {
       title: "UPhoto",
       summary: "testing summary",
       techStacks: ["a", "vv", "ccc"],
-      projectImage: "/../public/projectImages/portfolio-Uphoto.png",
+      projectImage: UPhotoImage,
       display: "https://ads1029.github.io/",
     },
     {
       title: "Pure Portfolio",
       summary: "testing summary",
       techStacks: ["HTML", "CSS"],
-      projectImage: "/../public/projectImages/portfolio-pure-portfolio.png",
+      projectImage: purePortfolioImage,
       githubLink: "https://ads1029.github.io/",
       display: "https://ads1029.github.io/",
     },
