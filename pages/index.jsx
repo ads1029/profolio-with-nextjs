8f9ddb083039b23c23c35cc6736054064091bb1b
Minjie Huang <minjie.huang@tigerspike.com>
2022-10-27 12:23:38 +1100

    adding threejs

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,50 @@
 import Head from "next/head";
 import Image from "next/image";
 import Navbar from "../src/components/Nav";
-import Counter, { countAtom } from "../src/components/Counter";
+import { Canvas } from "@react-three/fiber";
 import { useAtom } from "jotai";
 
 const Home = () => {
-  const [count, setCount] = useAtom(countAtom);
+  const Floor = (props) => {
+    return (
+      <mesh {...props} recieveShadow={true}>
+        <boxBufferGeometry args={[-10, 10, 30]} />
+        <meshPhysicalMaterial color="white" />
+      </mesh>
+    );
+  };
+
+  const Box = (props) => {
+    return (
+      <mesh {...props} recieveShadow={true} castShadow={true}>
+        <boxBufferGeometry />
+        <meshPhysicalMaterial color={"white"} />
+      </mesh>
+    );
+  };
 
   return (
     <div>
       <Navbar />
-      <h1 className="text-2xl"> {`<three.js model spinning>`} </h1>
-      <div className="flex flex-col">
-        <Counter />
-        <div>
-          I want to read atom too:
-          <div>the counter should also show here: {count}</div>
-        </div>
-        <button className="btn btn-primary btn-wide">Button</button>
-        <button className="btn btn-secondary btn-wide">Two</button>
+      <h1 className="text-4xl"> {`<three.js model spinning>`} </h1>
+
+      <Canvas
+        shadows={true}
+        className=""
+        camera={{
+          position: [-6, 7, 7],
+        }}
+      >
+        <ambientLight color={"pink"} intensity={0.7} />
+        <Box rotateX={3} rotateY={0.2} />
+        <Floor position={[0, -11, 10]} />
+      </Canvas>
+
+      <div className="intro prose-xl flex flex-col place-items-center">
+        <h1>Hi, I am Minjie</h1>
+        <h2>title h2</h2>
+        <h3>title h3</h3>
+        <p>pppppppp</p>
       </div>
     </div>
   );
